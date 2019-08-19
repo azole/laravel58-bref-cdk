8c50f2546fcb0210fe3193355f3bdfc59b2d13ff
azole <ashleylai58@gmail.com>
2019-08-19 15:04:42 +0800

    add lambda function with PHP Runtime Layer

--- lib/laravel58-cdk-deploy-stack.ts
+++ lib/laravel58-cdk-deploy-stack.ts
@@ -1,9 +1,18 @@
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
 
 export class Laravel58CdkDeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const phpRuntimeLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'lambda-php72-runtime', 'arn:aws:lambda:us-west-2:209497400698:layer:php-72-fpm:10');
+
+    const fn = new lambda.Function(this, 'CDK-Bref-fn', {
+        runtime: lambda.Runtime.PROVIDED,
+        code: lambda.Code.fromAsset('../laravel58-cdk'),
+        handler: 'public/index.php',
+        layers: [phpRuntimeLayer],
+    });
+
   }
 }
